dbbd70ef7faea4ce7dc017b83ffe049b728aca61
Cathy Dao <cathydao95@gmail.com>
2022-06-28 08:26:30 -0700

    add comments

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [input, setInput] = useState("");
   const [listItems, setListItems] = useState([]);
 
+  // function to set handle input change
   function handleChange(event) {
     setInput(event.target.value);
   }
 
+  // use input from handle change to automate shopping list items from API and set list
   useEffect(() => {
     fetch(`https://api.frontendeval.com/fake/food/${input}`)
       .then((res) => res.json())
@@ -23,6 +25,7 @@ function App() {
         <h1>My Shopping List</h1>
         <div className="input-container">
           <input type="text" onChange={handleChange} />
+          {/* pass list items from API to Autocomplete Component*/}
           <Autocomplete listItems={listItems} />
         </div>
       </div>
