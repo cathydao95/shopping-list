93e06c5512d4054aa4c2a68bba6b28f53a5f61bc
Cathy Dao <cathydao95@gmail.com>
2022-06-28 08:29:02 -0700

    add comments  to autocomplete component

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -4,10 +4,12 @@ import ListItems from "./ListItems";
 function Autocomplete({ listItems }) {
   const [selectedList, setSelectedList] = useState([]);
 
+  //   function to click on item on displayed list and create a new array with clicked(selected) items only
   function selectItem(item) {
     setSelectedList((current) => [...current, item]);
   }
 
+  //   check list received from API and map through each item and display on screen if list exists and greater than 0
   const displayList =
     listItems && listItems.length
       ? listItems.map((item, index) => {
@@ -22,6 +24,7 @@ function Autocomplete({ listItems }) {
   return (
     <div>
       <div className="automated-list">{displayList}</div>
+      {/* pass list of selectd item list to ListItems Component */}
       <ListItems selectedList={selectedList} setSelectedList={selectedList} />
     </div>
   );
