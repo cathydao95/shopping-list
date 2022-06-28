f998b114096592411e2f5adb161933228d97e8f1
Cathy Dao <cathydao95@gmail.com>
2022-06-27 21:07:30 -0700

    fetch data and handle input change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import Autocomplete from "./components/Autocomplete";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div></div>;
+  const [input, setInput] = useState("");
+  const [listItems, setListItems] = useState([]);
+
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
+
+  useEffect(() => {
+    fetch(`https://api.frontendeval.com/fake/food/${input}`)
+      .then((res) => res.json())
+      .then((data) => setListItems(data));
+  }, [input]);
+
+  console.log(listItems);
+
+  return (
+    <div>
+      <div className="container">
+        <h1>My Shopping List</h1>
+        <div className="input-container">
+          <input type="text" onChange={handleChange} />
+          <Autocomplete listItems={listItems} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
